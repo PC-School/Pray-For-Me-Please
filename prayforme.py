292ee965d825891f1c845def4b7eaf065a2d8aa2
Alona Ottieva <ottevaalena360@gmail.com>
2024-12-26 23:08:39 +0100

    make it better

--- prayforme.py
+++ prayforme.py
@@ -7,103 +7,53 @@ user_data = {}
 bot = telebot.TeleBot('7428507460:AAG-wGHPbIGYqvC0LS97Z9Tq4DTn6qHkaP8')
 
 @bot.message_handler(commands=['start'])
-def start(message):
-	conn = sqlite3.connect('needs.sql')
-#conn1 = sqlite3.connect('testimonies.sql')
-	cursor = conn.cursor()
-	cursor.execute('CREATE TABLE IF NOT EXISTS needs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, need TEXT NOT NULL)')
-	conn.commit()
-#conn1.commit()
-	cursor.close()
-	conn.close()
-#conn1.close()
+def askname(message):
+	bot.send_message(message.chat.id, "<b>Мир вам! Как вас зовут?</b>", parse_mode='HTML')
+	bot.register_next_step_handler(message, start)
 
+def start(message):
 	btn = types.InlineKeyboardMarkup()
+	user_data[message.chat.id] = {"name": message.text.strip()}
 	btn.add(types.InlineKeyboardButton("Помолитесь за меня", callback_data = 'prayer'))
-	btn.add(types.InlineKeyboardButton("Хочу поделиться свидетельством (не работает)", callback_data = 'testimony'))
-	bot.send_message(message.chat.id, '<b>Мир вам! Если есть молитвенная нужда, нажмите на кнопку "Помолитесь за меня" или напишите <code>/молитва</code> или /prayer\n\nЕсли хотите поделиться свидетельством, нажмите на кнопку "Хочу поделиться свидетельством" или напишите <code>/свидетельство</code> или /testimony</b>', parse_mode='HTML', reply_markup=btn)
+	btn.add(types.InlineKeyboardButton("Хочу поделиться свидетельством", callback_data = 'testimony'))
+	bot.send_message(message.chat.id, '<b>Если есть молитвенная нужда, нажмите на кнопку "Помолитесь за меня" или напишите <code>/молитва</code> или /prayer\n\nЕсли хотите поделиться свидетельством, нажмите на кнопку "Хочу поделиться свидетельством" или напишите <code>/свидетельство</code> или /testimony</b>', parse_mode='HTML', reply_markup=btn)
 	bot.send_message(message.chat.id, '<b>ЕСЛИ ВЫ ЗАМЕТИЛИ КАКОЙ-ТО БАГ, ПРИ НАПИСАНИИ ИЛИ ПУБЛИКАЦИИ НУЖДЫ ИЛИ СВИДЕТЕЛЬСТВА, ПИШИТЕ @alonagd17</b>', parse_mode='HTML')
 
 @bot.message_handler(commands=['молитва', 'prayer'])
-def askname(message):
-	bot.send_message(message.chat.id, "<b>Напишите своё имя</b>", parse_mode='HTML')
-	bot.register_next_step_handler(message, need)
-
 def need(message):
-	user_data[message.chat.id] = {"name": message.text.strip()}
 	bot.send_message(message.chat.id, '<b>Напишите свою нужду, если вы хотите, чтобы за вас просто помолились или не хотите называть нужду, то поставьте "-"</b>', parse_mode='HTML')
-	bot.register_next_step_handler(message, database_need)
+	bot.register_next_step_handler(message, send_need)
 
-def database_need(message):
+def send_need(message):
+	name = user_data.get(message.chat.id, {}).get("name", "Не указано")
 	need = message.text.strip()
 
-	if message.chat.id in user_data and "name" in user_data[message.chat.id]:
-		name = user_data[message.chat.id]["name"]
-	else:
-		name = "Не указано"
-
-	conn = sqlite3.connect('needs.sql')
-	cursor = conn.cursor()
-	cursor.execute('INSERT INTO needs (name, need) VALUES (?, ?)', (name, need))
-	conn.commit()
-
-	cursor.execute('SELECT * FROM needs WHERE id = (SELECT MAX(id) FROM needs)')
-	result = cursor.fetchone()
-
-	info = f"Имя: {result[1]}\nНужда: {result[2]}"
+	info = f"Молитвенная нужда:\n\nИмя: {name}\nНужда: {need}"
 	bot.send_message(message.chat.id, info)
 	bot.send_message(message.chat.id, '<b>Ваша нужда записана, вы можете увидеть её <a href="https://t.me/PrayForMePleaseChannel">тут</a>. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True)
-	channel_message = f"Молитвенная нужда:\n\nИмя: {result[1]}\nНужда: {result[2]}"
-	bot.send_message(-1002278314632, channel_message)
-	cursor.close()
-	conn.close()
+	bot.send_message(-1002278314632, info)
 
-"""
-@bot.message_handler(commands=['testimony', 'свидетельство'])
-def askname(message):
-	bot.send_message(message.chat.id, "<b>Напишите своё имя</b>", parse_mode='HTML')
-	bot.register_next_step_handler(message, testimony)
-
+@bot.message_handler(commands=['свидетельство', 'testimony'])
 def testimony(message):
-	user_data[message.chat.id] = {"name": message.text.strip()}
 	bot.send_message(message.chat.id, '<b>Опишите своё свидетельство как можно подробнее</b>', parse_mode='HTML')
-	bot.register_next_step_handler(message, database_testimony)
+	bot.register_next_step_handler(message, send_testimony)
 
-def database_testimony(message):
-	testimony = message.text.strip()
+def send_testimony(message):
+	name = user_data.get(message.chat.id, {}).get("name", "Не указано")
+	need = message.text.strip()
 
-	if message.chat.id in user_data and "name" in user_data[message.chat.id]:
-		name = user_data[message.chat.id]["name"]
-	else:
-		name = "Не указано"
-
-	conn1 = sqlite3.connect('testimony.sql')
-	cursor = conn1.cursor()
-	cursor.execute('INSERT INTO needs_testimony (name, testimony) VALUES (?, ?)', (name, testimony))
-	conn1.commit()
-
-	cursor.execute('SELECT * FROM needs_testimony WHERE id = (SELECT MAX(id) FROM needs_testimony)')
-	result = cursor.fetchone()
-
-
-	info = f"Имя: {result[1]}\nНужда: {result[2]}"
+	info = f"Свидетельство:\n\nИмя: {name}\nСвидетельство: {need}"
 	bot.send_message(message.chat.id, info)
 	bot.send_message(message.chat.id, '<b>Ваше свидетельство записано, вы можете увидеть его <a href="https://t.me/PrayForMePleaseChannel">тут</a>. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True)
-	channel_message = f"Свидетельство:\n\nИмя: {result[1]}\nСвидетельство: {result[2]}"
-	bot.send_message(-1002278314632, channel_message)
-	cursor.close()
-	conn1.close()
-"""
+	bot.send_message(-1002278314632, info)
+
 
 @bot.callback_query_handler(func = lambda callback: True)
 def callback_message(callback):
 	if callback.data == "prayer":
-		msg = bot.send_message(callback.message.chat.id, "<b>Напишите своё имя</b>", parse_mode='HTML')
-		bot.register_next_step_handler(msg, need)
-	"""
+		need(callback.message)
 	elif callback.data == "testimony":
 		msg = bot.send_message(callback.message.chat.id, "<b>Напишите своё имя</b>", parse_mode='HTML')
-		bot.register_next_step_handler(msg, testimony)
-	"""
+		testimony(callback.message)
 
 bot.polling(none_stop=True)
