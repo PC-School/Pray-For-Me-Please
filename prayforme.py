fcd1353764aa2d7b7be8e699fdac46dcaa1d9f8f
Alona Ottieva <ottevaalena360@gmail.com>
2025-02-07 13:22:23 +0100

    make it better

--- prayforme.py
+++ prayforme.py
@@ -1,10 +1,14 @@
 #main
 import telebot
 from telebot import types
+from pyTelegramBotCAPTCHA import CaptchaManager
 
 user_data = {}
+user_messages = {}
 
-bot = telebot.TeleBot('7839745229:AAGj_wwKyuRtVEc23RzZ3I8d1ZgsBXX0qgA')
+#bot = telebot.TeleBot('7916731915:AAHZxwTk_wDzb7vbmOhylKg8rVnjuZ2KF0Y') #test
+bot = telebot.TeleBot('7839745229:AAGj_wwKyuRtVEc23RzZ3I8d1ZgsBXX0qgA') #main
+captcha_manager = CaptchaManager(bot.get_me().id, default_timeout=90)
 #/main
 
 #start
@@ -18,83 +22,127 @@ def start(message):
 	btn = types.InlineKeyboardMarkup()
 	btn.row(types.InlineKeyboardButton("Молитва", callback_data='prayer'), types.InlineKeyboardButton("Свидетельство", callback_data='testimony'))
 	btn.row(types.InlineKeyboardButton("История покаяния", callback_data='repentance'), types.InlineKeyboardButton("Исповедь", callback_data='confession'))
-	bot.send_message(message.chat.id, '<b>Мир вам! 👼\n\nЕсли есть молитвенная нужда, нажмите на кнопку "Молитва"\n\nЕсли хотите поделиться свидетельством, нажмите на кнопку "Свидетельство"\n\nЕсли хотите рассказать историю о том, как вы пришли к Богу и покаялись, нажмите на кнопку "История покаяния"\n\nЕсли хотите исповедовать свой грех, нажмите на кнопку "Исповедь"</b>', parse_mode='HTML', reply_markup=btn)
+	sent = bot.send_message(message.chat.id, '<b>Мир вам! 👼\n\nЕсли есть молитвенная нужда, нажмите на кнопку "Молитва"\n\nЕсли хотите поделиться свидетельством, нажмите на кнопку "Свидетельство"\n\nЕсли хотите рассказать историю о том, как вы пришли к Богу и покаялись, нажмите на кнопку "История покаяния"\n\nЕсли хотите исповедовать свой грех, нажмите на кнопку "Исповедь"</b>', parse_mode='HTML', reply_markup=btn)
+	user_messages[message.chat.id] = sent.message_id
 	bot.send_message(message.chat.id, '<b>ЕСЛИ ВЫ ЗАМЕТИЛИ КАКОЙ-ТО БАГ ИЛИ ВОЗНИКЛИ ТРУДНОСТИ, ПИШИТЕ @alonagd17</b>', parse_mode='HTML')
 #/start
 
 #need
 def asknameprayer(message):
+	chat_id = message.chat.id
+	if chat_id in user_messages:
+		message_id = user_messages[chat_id]
+		bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=None)
 	bot.send_message(message.chat.id, '<b>Имя человека, за которого нужно помолиться: </b>', parse_mode='HTML')
 	bot.register_next_step_handler(message, need)
 
 def need(message):
 	name = message.text.strip()
 	user_data[message.chat.id] = {'name': name}
-	bot.send_message(message.chat.id, '<b>Напишите нужду. Если же вы хотите, чтобы просто упоминали в молитвах или не хотите называть нужду, то поставьте "-"</b>', parse_mode='HTML')
+	bot.send_message(message.chat.id, '<b>Напишите нужду. Если же вы хотите, чтобы просто упоминали в молитвах или не хотите называть нужду, то поставьте "-": </b>', parse_mode='HTML')
 	bot.register_next_step_handler(message, send_need)
 
 def send_need(message):
 	need = message.text.strip()
 	name = user_data.get(message.chat.id, {}).get('name', 'Не указано')
-	bot.send_message(message.chat.id, '<b>Ваша нужда записана, вы можете увидеть её <a href="https://t.me/PrayForMePleaseChannel">В МОЛИТВЕННОМ КАНАЛЕ</a> 👼. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True)
-	bot.send_message(-1002278314632, f"<b>Молитвенная нужда:\n\nИмя: </b><i>{name}</i>\n\n<b>Нужда: </b><i>{need}</i>", parse_mode='HTML')
+	markup = types.InlineKeyboardMarkup() 
+	start = types.InlineKeyboardButton("Вернуться в меню", callback_data='start')
+	markup.add(start)
+	bot.send_message(message.chat.id, '<b>Ваша нужда записана, вы можете увидеть её <a href="https://t.me/PrayForMePleaseChannel">В МОЛИТВЕННОМ КАНАЛЕ</a> 👼. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True, reply_markup=markup)
+	bot.send_message(-1002278314632, f"<b>#молитвенная_нужда\n\nИмя: </b><i>{name}</i>\n\n<b>Нужда: </b><i>{need}</i>", parse_mode='HTML')
 #/need
 
 #testimony
 def asknametestimony(message):
+	chat_id = message.chat.id
+	if chat_id in user_messages:
+		message_id = user_messages[chat_id]
+		bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=None)
 	bot.send_message(message.chat.id, '<b>Имя человека, о котором это свидетельство: </b>', parse_mode='HTML')
 	bot.register_next_step_handler(message, testimony)
 
 def testimony(message):
 	name = message.text.strip()
 	user_data[message.chat.id] = {'name': name}
-	bot.send_message(message.chat.id, '<b>Опишите свидетельство как можно подробнее</b>', parse_mode='HTML')
+	bot.send_message(message.chat.id, '<b>Опишите свидетельство как можно подробнее: </b>', parse_mode='HTML')
 	bot.register_next_step_handler(message, send_testimony)
 
 def send_testimony(message):
 	testimony = message.text.strip()
 	name = user_data.get(message.chat.id, {}).get('name', 'Не указано')
-	bot.send_message(message.chat.id, '<b>Ваше свидетельство записано, вы можете увидеть его <a href="https://t.me/PrayForMePleaseChannel">В МОЛИТВЕННОМ КАНАЛЕ</a> 👼. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True)
-	bot.send_message(-1002278314632, f"<b>Свидетельство:\n\nИмя: </b><i>{name}</i>\n\n<b>Свидетельство: </b><i>{testimony}</i>", parse_mode='HTML')
+	markup = types.InlineKeyboardMarkup() 
+	start = types.InlineKeyboardButton("Вернуться в меню", callback_data='start')
+	markup.add(start)
+	bot.send_message(message.chat.id, '<b>Ваше свидетельство записано, вы можете увидеть его <a href="https://t.me/PrayForMePleaseChannel">В МОЛИТВЕННОМ КАНАЛЕ</a> 👼. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True, reply_markup=markup)
+	bot.send_message(-1002278314632, f"<b>#свидетельство\n\nИмя: </b><i>{name}</i>\n\n<b>Свидетельство: </b><i>{testimony}</i>", parse_mode='HTML')
 #/testimony
 
 #repentance
 def asknamerepentance(message):
+	chat_id = message.chat.id
+	if chat_id in user_messages:
+		message_id = user_messages[chat_id]
+		bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=None)
 	bot.send_message(message.chat.id, '<b>Имя покаявшегося человека: </b>', parse_mode='HTML')
 	bot.register_next_step_handler(message, repentance)
 
 def repentance(message):
 	name = message.text.strip()
 	user_data[message.chat.id] = {'name': name}
-	bot.send_message(message.chat.id, '<b>Опишите свою историю покаяния</b>', parse_mode='HTML')
+	bot.send_message(message.chat.id, '<b>Опишите историю покаяния: </b>', parse_mode='HTML')
 	bot.register_next_step_handler(message, send_repentance)
 
 def send_repentance(message):
 	repentance = message.text.strip()
 	name = user_data.get(message.chat.id, {}).get('name', 'Не указано')
-	bot.send_message(message.chat.id, '<b>Ваша история записана, вы можете увидеть её <a href="https://t.me/PrayForMePleaseChannel">В МОЛИТВЕННОМ КАНАЛЕ</a> 👼. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True)
-	bot.send_message(-1002278314632, f"<b>История покаяния:\n\nИмя: </b><i>{name}</i>\n\n<b>История: </b><i>{repentance}</i>", parse_mode='HTML')
+	markup = types.InlineKeyboardMarkup() 
+	start = types.InlineKeyboardButton("Вернуться в меню", callback_data='start')
+	markup.add(start)
+	bot.send_message(message.chat.id, '<b>Ваша история записана, вы можете увидеть её <a href="https://t.me/PrayForMePleaseChannel">В МОЛИТВЕННОМ КАНАЛЕ</a> 👼. А так же вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True, reply_markup=markup)
+	bot.send_message(-1002278314632, f"<b>#история_покаяния\n\nИмя: </b><i>{name}</i>\n\n<b>История: </b><i>{repentance}</i>", parse_mode='HTML')
 #/repentance
 
 #confession
 def asknameconfession(message):
-	bot.send_message(message.chat.id, '<b>Имя человека, который исповедуется: </b>', parse_mode='HTML')
-	bot.register_next_step_handler(message, confession)
+	chat_id = message.chat.id
+	if chat_id in user_messages:
+		message_id = user_messages[chat_id]
+		bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=None)
+	bot.send_message(message.chat.id, '<b>Укажите ваше имя: </b>', parse_mode='HTML')
+	bot.register_next_step_handler(message, gender)
 
-def confession(message):
+def gender(message):
 	name = message.text.strip()
+	btn = types.InlineKeyboardMarkup()
+	btn.row(types.InlineKeyboardButton("Женский", callback_data='woman'), types.InlineKeyboardButton("Мужской", callback_data='man'))
+	bot.send_message(message.chat.id, '<b>Укажите ваш пол: </b>', parse_mode='HTML', reply_markup=btn)
 	user_data[message.chat.id] = {'name': name}
-	bot.send_message(message.chat.id, '<b>Опишите ваш грех</b>', parse_mode='HTML')
-	bot.register_next_step_handler(message, send_confession)
 
-def send_confession(message):
+def confessionman(message):
+	bot.send_message(message.chat.id, '<b>Опишите ваш грех: </b>', parse_mode='HTML')
+	bot.register_next_step_handler(message, sendconfessionman)
+
+def confessionwoman(message):
+	bot.send_message(message.chat.id, '<b>Опишите ваш грех: </b>', parse_mode='HTML')
+	bot.register_next_step_handler(message, sendconfessionwoman)
+
+def sendconfessionwoman(message):
 	confession = message.text.strip()
-	user = message.from_user
-	userid = message.from_user.id
 	name = user_data.get(message.chat.id, {}).get('name', 'Не указано')
-	username = user.username if user.username else userid
+	markup = types.InlineKeyboardMarkup() 
+	start = types.InlineKeyboardButton("Вернуться в меню", callback_data='start')
+	markup.add(start)
 	bot.send_message(message.chat.id, '<b>Ваша исповедь записана и передана админу 👼. Вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True)
-	bot.send_message(-1002408173767, f"<b>Исповедание греха:\n\nИмя: </b><i>{name}</i>\n\n<b>Юзер: </b><i>{username}</i>\n\n<b>Исповедь: </b><i>{confession}</i>", parse_mode='HTML')
+	bot.send_message(-1002408173767, f"<b>Имя: </b><i>{name}</i>\n\n<b>Исповедь: </b><i>{confession}</i>", parse_mode='HTML')
+
+def sendconfessionman(message):
+	confession = message.text.strip()
+	name = user_data.get(message.chat.id, {}).get('name', 'Не указано')
+	markup = types.InlineKeyboardMarkup() 
+	start = types.InlineKeyboardButton("Вернуться в меню", callback_data='start')
+	markup.add(start)
+	bot.send_message(message.chat.id, '<b>Ваша исповедь записана и передана админу 👼. Вы можете помолиться за других, как сказано:</b> \n\n<blockquote>Признавайтесь друг пред другом в проступках и молитесь друг за друга, чтобы исцелиться: много может усиленная молитва праведного.\n\nПослание Иакова 5:16</blockquote>', parse_mode='HTML', disable_web_page_preview=True)
+	bot.send_message(-1002430908090, f"<b>Имя: </b><i>{name}</i>\n\n<b>Исповедь: </b><i>{confession}</i>", parse_mode='HTML')
 #/confession
 
 #buttons
@@ -108,6 +156,12 @@ def callback_message(callback):
 		asknamerepentance(callback.message)
 	elif callback.data == "confession":
 		asknameconfession(callback.message)
+	elif callback.data == "woman":
+		confessionwoman(callback.message)
+	elif callback.data == "man":
+		confessionman(callback.message)
+	elif callback.data == "start":
+		start(callback.message)
 	elif callback.data == "checksubscribe":
 		status = bot.get_chat_member("@PrayForMePleaseChannel", callback.from_user.id).status
 		if status in ['member', 'administrator', 'creator']:
